74c6d90b28c7255e7a34ca1604eab1fb041212a7
Jai Mangat <jm@jais-mbp.mynetworksettings.com>
2025-10-28 19:06:01 -0400

    Fix theme provider props

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import type { ThemeProviderProps } from "next-themes";
+import type { ComponentProps } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+type NextThemesProviderProps = ComponentProps<typeof NextThemesProvider>;
+
+export function ThemeProvider({ children, ...props }: NextThemesProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
